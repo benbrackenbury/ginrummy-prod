037aa14fe94a8d8fcc148f41b5c4323c3595127e
Ben Brackenbury <benbrack22@icloud.com>
2021-07-22 17:46:24 +0100

    first commit

---
+++ precache-manifest.d629de000d6bf7c9edab55b74f77638b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b6a6b90312e2334922445555417ca7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "982d6fc161f1122f5a4e",
+    "url": "./static/css/main.e02cca2d.chunk.css"
+  },
+  {
+    "revision": "9b510deae398312a02c6",
+    "url": "./static/js/2.644e9020.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "./static/js/2.644e9020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982d6fc161f1122f5a4e",
+    "url": "./static/js/main.f7a142b3.chunk.js"
+  },
+  {
+    "revision": "8dc02e9663792f7718a9",
+    "url": "./static/js/runtime-main.8114aaab.js"
+  }
+]);
